55b19ca9008433d399fad0cab28436c2ac13ea3d
Pawel Ptaszynski <pptaszynski@gmail.com>
2008-06-11 19:53:28 +0200

    A asm source file with processing function. It doesn't really work. The reason isn't known.

--- proj1.asm
+++ proj1.asm
@@ -5,15 +5,11 @@
 %define CYCLEBUFFLAST	[ebp-20]
 %define X			[ebp-24]
 %define Y			[ebp-28]
-%define TOPDST		[ebp-32]
-%define	BOTTOMDST	[ebp-36]
-%define EXTENDBOTEDGE	[ebp-40]
+;%define TOPDST		[ebp-32]
 %define BSUM		[ebp-44]
 %define RSUM		[ebp-48]
 %define GSUM		[ebp-52]
 %define ROWL		[ebp-56]
-%define BUFFOFFSET	[ebp-60]
-%define	DENOM		[ebp-64]
 %define	MASKBEGADR	[ebp-68]		
 %define	AVGB		[ebp-72]
 %define AVGR		[ebp-76]
@@ -21,8 +17,9 @@
 %define	PREV		[ebp-84]
 %define	BTRPT		[ebp-88]
 %define BUFFPOS		[ebp-92]
-%define	DEST		[ebp-96]
+%define	DSTPOS		[ebp-96]
 %define	SRCPOS		[ebp-100]
+
 global rectBlur
 global getPixel
 extern malloc
@@ -82,7 +79,7 @@ rectBlur:
 		push	ecx
 		push	eax
 		call	malloc					; Zwraca adres zaalokowanego obszaru w eax
-		sub		esp, -4
+		add		esp, 4
 		pop		ecx
 		mov		CYCLEBUFF, eax
 		sub		ecx, 4
@@ -92,21 +89,21 @@ rectBlur:
 		mov		Y, dword 0x0
 		mov		X, dword 0x0
 		
+		mov		ecx, [ebp+8]
+		mov		SRCPOS, ecx
+		mov		ecx, [ebp+20]
+		mov		DSTPOS, ecx
 		; ESI - *src
 		; EDI - *dst
-
-		mov		ecx, [ebp+16]			; zapisz w ecx imgheight
 
 		; ECX - height
 
 		_vert:
-				mov		eax, [ebp+16]	; zapisz w eax imgheight
-				sub		eax, ecx		; Oblicz odleglosc od dolnej krawedzi (przy zalozeniu ze obraz przegladamy od dolu)
+				mov		eax, Y	; wysokosc w obrazku
+				;sub		eax, ecx		; Oblicz odleglosc od dolnej krawedzi (przy zalozeniu ze obraz przegladamy od dolu)
 		; EAX - Odległość od dolnej krawędzi
-				mov		BOTTOMDST, eax	; Zapisz jako zm lokalna
-				push	ecx				; PUSH ecx na stos -> ECX zawiera HEIGHT
 		
-		; PUSH ECX -> ECX na stos
+		; PUSH ECX -> ECX na stos - LICZNIK PETLI PRZEBIEGU PIONOWEGO
 
 			_fillbuff:				
 				;; PREPARE A CYCLE BUFFOR WITH COMPTUED AVERAGE
@@ -114,21 +111,20 @@ rectBlur:
 
 				; sub		eax, [ebp+28]
 				makebuff:
+				; COMPUTE THEORETICAL ADRES TO A FIRST PIXEL UNDER FILTER MASK
 				mov		edx, [ebp+28]
+				mov		eax, Y
 				sub		edx, eax
 				xor		ebx, ebx
 				cmp		edx, ebx
 				cmovg   ebx, edx
 				mov		BTRPT, ebx
 				mov		esi, [ebp+8]
-				mov		ebx, esi
 				mov		edx, ROWL
-				push	eax
-				mov		eax, [ebp+28]
-				mul		edx
-				sub		ebx, eax
-				mov		esi, ebx
-				pop		eax
+				; push	eax
+				sub		eax, [ebp+28]
+				imul	edx
+				add		esi, eax
 
 				mov		MASKBEGADR, esi
 				makebuffExtEdge:
@@ -136,79 +132,88 @@ rectBlur:
 					mov		BSUM, ecx
 					mov		RSUM, ecx
 					mov		GSUM, ecx
-					mov		ecx, FHEIGHT
-					xor		eax, eax	
+					mov		ecx, dword FHEIGHT
+					xor		eax, eax
 					countEdgeValues:
+						push	ecx
+						mov		ecx, eax
 						mov		esi, MASKBEGADR
+						mul		edx
+						add		esi, eax
+						mov		eax, ecx
 						mov		edi, [ebp+8]
 						xor		ebx, ebx
 						cmp		esi, edi
 						cmovl   eax, ebx
-						add		eax, BTRPT
-						mov		ebx, esi
+						cmovl   ebx, BTRPT
+						add		eax, ebx
 						push	eax
 						mul		edx
-						lea		esi, [ebx+eax]
+						add		esi, eax
 						pop		eax
 						mov		ebx, IMGSIZE
 						add		edi, ebx
 						cmp		esi, edi
 						cmovge	esi, PREV
 						mov		PREV, esi
-						mov		ebx, [esi]	; EBX - CAŁY PIKSEL B-R-G-A
+						lodsd
+						; EBX - CAŁY PIKSEL B-R-G-A
 						; push	eax			; EAX - LICZNIK WYSOKOSCI W KOLUMNIE -> STOS
 
-						mov		edi, ebx
-						; ADD B VALUE
-						and		edi, 0xFF000000
-						shr		edi, 24
-						add		BSUM, edi
-						; ADD R VALUE
-						mov		edi, ebx
-						and		edi, 0x00FF0000
-						shr		edi, 16
-						add		RSUM, edi
+						mov		ebx, eax
 						; ADD G VALUE
-						mov		edi, ebx
-						and		edi, 0x0000FF00
-						shr		edi, 8
-						add		GSUM, edi
+						and		ebx, 0x00FF0000
+						shr		ebx, 16
+						add		GSUM, ebx
+						; ADD R VALUE
+						mov		ebx, eax
+						and		ebx, 0x0000FF00
+						shr		ebx, 8
+						add		RSUM, ebx
+						; ADD B RALUE
+						mov		ebx, eax
+						and		ebx, 0x000000FF
+						;shr		edi, 8
+						add		BSUM, ebx
 						; pop		eax			; EAX - LICZNIK WYSOKOSCI W KOLUMNE <- STOS
-						sub		eax, BTRPT
-						inc		eax	
+						mov		eax, ecx
+						inc		eax
+						pop		ecx
 					loop	countEdgeValues
 					
-					mov		edi, CYCLEBUFF
 					; DZIELIMY WARTOSCI 
-					mov		eax, dword BSUM		; BLUE 
+					xor		ebx, ebx
+					mov		eax, dword GSUM		; GREEN 
 					mov		ecx, dword FHEIGHT
-					div		ecx 
-					mov		ah, al
-					mov		[edi], al		; Zapisujemy w buforze
-						
+					cdq
+					idiv	ecx 
+					shl		eax, 16
+					and		eax, 0x00FF0000
+					or		ebx, eax
+
 					mov		eax, dword RSUM		; RED
-					mov		ecx, FHEIGHT
-					div		ecx
-					mov		bl, al
-					mov		[edi+1], al		; Zapisujemy w buforze
+					mov		ecx, dword FHEIGHT
+					cdq
+					idiv		ecx
+					shl		eax , 8
+					and		eax, 0x0000FF00
+					or		ebx, eax
 						
-					mov		eax, dword GSUM		; GREEN
-					mov		ecx, FHEIGHT	
+					mov		eax, dword BSUM		; BLUE
+					mov		ecx, dword FHEIGHT	
 					div		ecx
-					mov		bh, al
-					mov		[edi+2], al		; Zapisujemy w buforze
-						
-					add		edi, 0x4
+					and		eax, 0x000000FF
+					or		eax, ebx
+
+					mov		edi, dword CYCLEBUFF
+					stosd		
 					;; WYPELNIANIE POCZATKU BUFORA - ROZSZERZANIE KRAWEDZI
 					mov		ecx, [ebp+24]
 					extLEdgeLoop:
-						mov		[edi], ah
-						mov		[edi+1], bl
-						mov		[edi+2], bh
-						add		edi, 0x4
-						mov		BUFFPOS, edi
+						stosd
 					loop	extLEdgeLoop
 					
+					mov		BUFFPOS, edi
 				;; NOW WE HAVE FILLED FIRST W+1 POS. OF CYCLEBUFFER FILLED WITH
 				;; THE AVERAGE VALUES OF LEF EDGE PIXELS
 
@@ -229,38 +234,26 @@ rectBlur:
 					mov		BSUM, ecx
 					mov		RSUM, ecx
 					mov		GSUM, ecx
-					mov		ecx, FHEIGHT
+					mov		ecx, dword FHEIGHT
 					xor		eax, eax
 					mov		edx, ROWL
 					push	esi				; ESI -> STOS
 					makeBuffYLoop:
-						;mov		esi, MASKBEGADR
-						;mov		edi, [ebp+8]
-						;xor		ebx, ebx
-						;cmp		esi, edi
-						;cmovl   eax, ebx
-						;add		eax, BTRPT
-						;mov		ebx, esi
-						;push	eax
-						;mul		edx
-						;lea		esi, [ebx+eax]
-						;pop		eax
-						;mov		ebx, IMGSIZE
-						;add		edi, ebx
-						;cmp		esi, edi
-						;cmovge	esi, PREV
-						;mov		PREV, esi
-						;mov		ebx, [esi]	; EBX - CAŁY PIKSEL B-R-G-A
+						push ecx
+						mov		ecx, eax
 						mov		esi, MASKBEGADR
+						mul		edx
+						add		esi, eax
+						mov		eax, ecx
 						mov		edi, [ebp+8]
 						xor		ebx, ebx
 						cmp		esi, edi
 						cmovl   eax, ebx
-						add		eax, BTRPT
-						mov		ebx, esi						
+						cmovl	ebx, BTRPT
+						add		eax, ebx						
 						push	eax
 						mul		edx
-						lea		esi, [esi+eax]
+						add		esi, eax
 						pop		eax
 						mov		ebx, IMGSIZE
 						add		edi, ebx
@@ -271,66 +264,66 @@ rectBlur:
 						;mul		edx
 						;lea		esi, [ebx+eax]
 						;pop		eax
-						mov		ebx, [esi]	; EBX - CAŁY PIKSEL B-R-G-A
+						lodsd	; EBX - CAŁY PIKSEL B-R-G-A
 						;pop		esi
 						;push	eax			; EAX - LICZNIK WYSOKOSCI W KOLUMNIE -> STOS
 
-						mov		edi, ebx	
-						; ADD B VALUE
-						and		edi, 0xFF000000
-						shr		edi, 24
-						add		BSUM, edi
-						; ADD R VALUE
-						mov		edi, ebx
-						and		edi, 0x00FF0000
-						shr		edi, 16
-						add		RSUM, edi
+						mov		ebx, eax	
 						; ADD G VALUE
-						mov		edi, ebx
-						and		edi, 0x0000FF00
-						shr		edi, 8
-						add		GSUM, edi
-
-						sub		eax, BTRPT
+						and		ebx, 0x00FF0000
+						shr		ebx, 16
+						add		GSUM, ebx
+						; ADD R VALUE
+						mov		ebx, eax
+						and		ebx, 0x0000FF00
+						shr		ebx, 8
+						add		RSUM, ebx
+						; ADD G VALUE
+						mov		ebx, eax
+						and		ebx, 0x000000FF
+						;shr		ebx, 8
+						add		BSUM, ebx
+						
+						mov		eax, ecx
 						inc		eax
+						pop		ecx
 					loop	makeBuffYLoop
 					pop		esi
+					xor		ebx, ebx
 					; DZIELIMY WARTOSCI 
-					mov		edi, BUFFPOS
-					mov		eax, BSUM
-					mov		ecx, FHEIGHT
-					div		ecx
-					mov		[edi], al		; Zapisujemy w buforze
+					mov		eax, GSUM
+					mov		ecx, dword FHEIGHT
+					cdq
+					idiv	ecx
+					shl		eax, 16
+					and		eax, 0X00FF0000	
+					or		ebx, eax		; Zapisujemy w buforze
 
 					mov		eax, RSUM
-					mov		ecx, FHEIGHT
+					mov		ecx, dword FHEIGHT
 					div		ecx
-					mov		[edi+1], al	; Zapisujemy w buforze
+					shl		eax, 8
+					and		eax, 0x0000FF00
+					or		ebx, eax
 					
-					mov		eax, GSUM
-					mov		ecx, FHEIGHT
-					div		ecx
-					mov		[edi+2], al	; Zapisujemy w buforze
+					mov		eax, BSUM
+					mov		ecx, dword FHEIGHT
+					;shl		eax, 8
+					and		eax, 0x000000FF
+					or		eax, ebx
+					mov		edi, BUFFPOS
+					mov		eax, ebx
+					stosd
 
-					add		edi, 0x4
 					mov		BUFFPOS, edi
 
 					pop		ecx
 					pop		eax
 					sub		ecx, 1
 				jnz	makeBuffXLoop
+
 				mov		esi, CYCLEBUFF
 				mov		BUFFPOS, esi
-				mov	    esi, [ebp+8]
-				mov		ecx, X
-				mov		eax, Y
-				mov		edx, ROWL
-				imul		ecx
-				add		esi, eax
-				add		esi, ecx
-				; lea		esi,[ebx+eax+ecx]
-				mov		SRCPOS, esi
-				mov		ecx, [ebp+12]
 				xor		eax, eax
 				mov		X, eax
 				_hor:
@@ -339,80 +332,76 @@ rectBlur:
 					mov		BSUM, ebx
 					mov		RSUM, ebx
 					mov		GSUM, ebx
-					push	ecx				; ECX -> STOS
-					
-					mov		ecx, FWIDTH
+					mov		esi, CYCLEBUFF
 					_sumBuff:
-						mov	esi, CYCLEBUFF
-						mov ebx, [esi]
+						lodsd
 						
-						mov		eax, ebx	
+						mov		ebx, eax	
 						; ADD B VALUE
-						and		eax, 0xFF000000
-						shr		eax, 24
-						add		BSUM, eax
+						and		ebx, 0x00FF0000
+						shr		ebx, 16
+						add		GSUM, ebx
 						; ADD R VALUE
-						mov		eax, ebx
-						and		eax, 0x00FF0000
-						shr		eax, 16
-						add		RSUM, eax
+						mov		ebx, eax
+						and		ebx, 0x0000FF00
+						shr		ebx, 8
+						add		RSUM, ebx
 						; ADD G VALUE
-						mov		eax, ebx
-						and		eax, 0x0000FF00
-						shr		eax, 8
-						add		GSUM, eax
-						
-						add		esi, 0x04
-					loop	_sumBuff
+						mov		ebx, eax
+						and		ebx, 0x000000FF
+						;shr		eax, 8
+						add		BSUM, ebx
+						cmp		esi, CYCLEBUFFLAST
+					jl		_sumBuff
+
 						xor		ebx, ebx
 						; DZIELIMY WARTOSCI 
-						mov		eax, BSUM
-						mov		ecx, FWIDTH
-						div		ecx
-						shl		eax, 24
-						and		eax,0xFF000000
-						or		ebx, eax		; Zapisujemy w buforze
-						
-						mov		eax, RSUM
-						mov		ecx, FWIDTH
-						div		ecx
+						mov		eax, dword GSUM
+						mov		ecx, dword FWIDTH
+						cdq
+						idiv		ecx
 						shl		eax, 16
-						and		eax, 0x00FF0000
-						or		ebx, eax
+						and		eax,0x00FF0000
+						or		ebx, eax		; Zapisujemy w ebx
 						
-						mov		eax, GSUM
-						mov		ecx, FWIDTH
-						div		ecx
+						mov		eax, dword RSUM
+						mov		ecx, dword FWIDTH
+						cdq
+						idiv	ecx
 						shl		eax, 8
 						and		eax, 0x0000FF00
 						or		ebx, eax
 						
-						mov		esi, SRCPOS
-						mov		eax, [esi]
+						mov		eax, dword BSUM
+						mov		ecx, dword FWIDTH
+						cdq
+						idiv		ecx
+						;shl		eax, 8
 						and		eax, 0x000000FF
-						and		ebx, eax
+						or		ebx, eax
 						
-						push	ebx
-						push	edx
-						push	ecx
+						mov		esi, dword SRCPOS
+						lodsd
+						and		eax, 0xFF000000
+						or		eax, ebx
+						
+						mov		SRCPOS, esi
 
-						mov	    edi, [ebp+20]
-						mov		ecx, X
-						mov		eax, Y
-						mov		edx, ROWL
-						mul		edx
-						add		edi, eax
-						add		edi, ecx
+						mov	    edi, dword DSTPOS
+						;mov		ecx, X
+						;shl		ecx, 2
+						;mov		eax, Y
+						;mov		edx, ROWL
+						;mul		edx
+						;add		edi, eax
+						;add		edi, ecx
 						; lea		edi,[ebx+eax+ecx]
 
-						mov		eax, ebx
-						stosb
-
-						pop		ecx
-						pop		edx
-						pop		ebx
+						stosd
+						mov		DSTPOS, edi
 						
 						mov		ecx, [ebp+12]
+						dec		ecx
 						mov		eax, X
 						add		eax, [ebp+24]
 						cmp		eax, ecx
@@ -420,33 +409,41 @@ rectBlur:
 							mov		esi, CYCLEBUFFLAST
 							mov		eax, [esi]
 							mov		edi, BUFFPOS
-							stosb
+							stosd
 							cmp		edi, CYCLEBUFFLAST
-							cmovg	edi, CYCLEBUFF
-							mov		edi, BUFFPOS
+							cmovge	edi, CYCLEBUFF
+							mov		BUFFPOS, edi
 							jmp		addedToBuff
 						;; REPLACE NEXT COLUMN IN BUFFER
 						addToBuffLoopPROL:
-						mov		esi, SRCPOS
+						mov		esi, MASKBEGADR
 						mov		eax, [ebp+24]
+						inc		eax
 						shl		eax, 2
 						add		esi, eax
-						mov		eax, [ebp+28]
-						mov		edx, ROWL
-						mul		edx
-						sub		esi, eax
-						mov		ecx, FHEIGHT
+						;mov		eax, [ebp+28]
+						;mov		edx, ROWL
+						;mul		edx
+						;sub		esi, eax
+						mov		ecx, dword FHEIGHT
 						xor		eax, eax
 						addToBuffYLoop:
+							push ecx
+							mov		ecx, eax
+							mov		esi, MASKBEGADR
+							mul		edx
+							add		esi, eax
+							mov		eax, ecx						
 							mov		edi, [ebp+8]
 							xor		ebx, ebx
 							cmp		esi, edi
 							cmovl   eax, ebx
-							add		eax, BTRPT
-							mov		ebx, esi
+							cmovl	ebx, BTRPT
+							add		eax, ebx
 							push	eax
 							mul		edx
-							lea		esi, [ebx+eax]
+							add		esi, eax
+							pop		eax
 							mov		ebx, IMGSIZE
 							add		edi, ebx
 							cmp		esi, edi
@@ -454,65 +451,76 @@ rectBlur:
 							mov		PREV, esi
 							;push	esi
 							;lea		esi, [ebx+eax]
-							mov		ebx, [esi]	; EBX - CAŁY PIKSEL B-R-G-A
+							;mov		ebx, [esi]	; EBX - CAŁY PIKSEL B-R-G-A
 							;pop		esi
 							; push	eax			; EAX - LICZNIK WYSOKOSCI W KOLUMNIE -> STOS
+							lodsd
 
-							mov		edi, ebx	
-							; ADD B VALUE
-							and		edi, 0xFF000000
-							shr		edi, 24
-							add		BSUM, edi
-							; ADD R VALUE
-							mov		edi, ebx
-							and		edi, 0x00FF0000
-							shr		edi, 16
-							add		RSUM, edi
+							mov		ebx, eax	
 							; ADD G VALUE
-							mov		edi, ebx
-							and		edi, 0x0000FF00
-							shr		edi, 8
-							add		GSUM, edi
+							and		ebx, 0x00FF0000
+							shr		ebx, 16
+							add		GSUM, ebx
+							; ADD R VALUE
+							mov		ebx, eax
+							and		ebx, 0x0000FF00
+							shr		ebx, 8
+							add		RSUM, ebx
+							; ADD G VALUE
+							mov		ebx, eax
+							and		ebx, 0x000000FF
+							;shr		edi, 8
+							add		GSUM, ebx
 							
-							pop		eax
-							sub		eax, BTRPT
+							mov		eax, ecx
+							;sub		eax, BTRPT
 							inc		eax
+							pop		ecx
 						loop	addToBuffYLoop
 						; DZIELIMY WARTOSCI
-						mov		edi, BUFFPOS
-						mov		eax, BSUM
-						mov		ecx, FHEIGHT
-						div		ecx
-						mov		[edi], al		; Zapisujemy w buforze
-						
-						mov		eax, RSUM	
-						mov		ecx, FHEIGHT
-						div		ecx
-						mov		[edi+1], al	; Zapisujemy w buforze
 						
 						mov		eax, GSUM
-						mov		ecx, FHEIGHT
-						div		ecx
-							
-						mov		[edi+2], al	; Zapisujemy w buforze
+						mov		ecx, dword FHEIGHT
+						cdq
+						idiv		ecx
+						shl		eax, 16
+						and		eax, 0X00FF0000	
+						or		ebx, eax		; Zapisujemy w buforze
 
-						add		edi, 0x4
+						mov		eax, RSUM
+						mov		ecx, dword FHEIGHT
+						cdq
+						idiv		ecx
+						shl		eax, 8
+						and		eax, 0x0000FF00
+						or		ebx, eax
+					
+						mov		eax, GSUM
+						mov		ecx, dword FHEIGHT
+						;shl		eax, 8
+						and		eax, 0x000000FF
+						or		eax, ebx
+						mov		edi, BUFFPOS
+						stosd
+						
 						cmp		edi, CYCLEBUFFLAST
 						cmovg	edi, CYCLEBUFF
 						mov		edi, BUFFPOS
+						mov		BUFFPOS, edi
+						
 						addedToBuff:
 						mov		eax, X
 						inc		eax
 						mov		X, eax
-						pop		ecx
-						sub		ecx, 1
-						jnz		_hor
-				pop		ecx
+						mov		ecx, [ebp+12]
+						cmp		eax, ecx
+						jl		_hor
 				mov		eax, Y
 				inc		eax
 				mov		Y, eax
-				sub		ecx, 1
-				jnz		_vert
+				mov		ecx, [ebp+16]
+				cmp		eax, ecx
+				jl		_vert
 
 
 end:
@@ -531,7 +539,7 @@ end:
 		leave
         ret
 
-_getPixel:
+getPixel:
 		; Prolog
 		push	ebp
         mov		ebp, esp
